90837b8de3dcf95229204aa999663e3fb3f277a4
Jxn <jackson.onyango@gmail.com>
2017-02-06 16:08:22 +0300

    Add prime_numbers.py and tests_prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,7 @@
+'''
+    Generates prime numbers in a range between 0 and a given positve number
+    Args:
+        Takes 1 argument for the upper limit of the range
+    Returns:
+        Returns a list containing all prime numbers in the range
+'''
---
+++ tests_prime_numbers.py
@@ -0,0 +1,3 @@
+'''
+    Tests for the prime_numbers() function
+'''
