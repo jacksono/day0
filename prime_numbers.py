89d5671115c5bd8dd2fb3e54a0e5d0cb2c0d467b
Jxn <jackson.onyango@gmail.com>
2017-02-06 16:53:24 +0300

    Pass the first 7 tests

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,7 +1,14 @@
-'''
+
+def prime_numbers(n):
+    '''
     Generates prime numbers in a range between 0 and a given positve number
     Args:
         Takes 1 argument for the upper limit of the range
     Returns:
         Returns a list containing all prime numbers in the range
-'''
+    '''
+    if not isinstance(n, int):
+        return "Invalid input type"
+    if n < 0:
+        return 'Invalid input, negative number entered'
+
